60885859d7bc12530e1ae2f3c9648ee965f6f14c
Harsh Srivastava <srivastavah240@gmail.com>
2025-03-21 15:30:59 +0530

    (feat): Created component to sync user with convex , added backend api routes to update user , get user by id

---
+++ src/components/SyncUserWithConvex.tsx
@@ -0,0 +1,38 @@
+
+import { useUser } from '@clerk/nextjs'
+import { useMutation } from 'convex/react';
+import React, { useEffect } from 'react'
+import { api } from '../../convex/_generated/api';
+
+type Props = {}
+
+const SyncUserWithConvex = (props: Props) => {
+
+    const {user}=useUser();
+    const updatedUser=useMutation(api.users.updateUser);
+
+    useEffect(() => {
+
+        if(!user)
+        {
+            return;
+        }
+        const syncUser=async()=>{
+            try {
+                await  updatedUser({
+                    userId:user.id ,
+                    name:`${user.firstName ?? ""} ${user.lastName ?? ""}.trim()`,
+                    email:user.emailAddresses[0]?.emailAddress ?? "",
+                })
+            } catch (error) {
+                console.error("Error synching with user:",error);
+            }
+        }
+        syncUser();
+     
+    }, [user ,updatedUser])
+    
+  return  null ;
+}
+
+export default SyncUserWithConvex

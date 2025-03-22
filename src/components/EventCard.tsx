777ed6c1eca0803888168e2ee0789d4340bc4cf3
Harsh Srivastava <srivastavah240@gmail.com>
2025-03-22 19:02:57 +0530

    (feat):Fetched events information from convex backend apis

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,11 +1,46 @@
+"use client";
 import React from 'react'
 import { Id } from '../../convex/_generated/dataModel'
+import { useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation';
+import { useQuery } from 'convex/react';
+import { api } from '../../convex/_generated/api';
+import { useStorageUrl } from '@/lib/utils';
 
 type Props = {}
 
 const EventCard = ({eventId}:{
     eventId:Id<"events">
 }) => {
+
+  const {user}=useUser();
+  const {router}=useRouter();
+  const event =useQuery(api.events.getById , {
+    eventId
+  });
+  //get avaliability of tickets
+  const availability= useQuery(api.events.getEventAvaliability , {
+    eventId
+  });
+  // Check  wheteher user has purchased ticket for this event or not
+  const userTicket=useQuery(api.tickets.getUserTicketForEvent , {
+      eventId  , 
+      userId:user?.id ?? "",
+  })
+
+  const queuePosition=useQuery(api.waitingList.getQueuePosition , {
+    eventId ,
+    userId: user?.id ?? "",
+  });
+  const imageUrl= useStorageUrl(event?.imageStorageId);
+
+  if(!event  ||!availability)
+  {
+    return  null ;
+  }
+  
+  const isPastEvent= event?.eventDate <=Date.now();
+  const isEventOwner= event?.userId ===user?.id;
   return (
     <div>EventCard</div>
   )
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
+import { useQuery } from "convex/react";
 import { twMerge } from "tailwind-merge"
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+export function useStorageUrl(storageId: Id<"_storage"> | undefined) {
+  return useQuery(api.storage.getUrl, storageId ? { storageId } : "skip");
 }

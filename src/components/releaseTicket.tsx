1e1f010048e7105e76b2710e9f5bd20b0db15c78
Harsh Srivastava <srivastavah240@gmail.com>
2025-03-27 18:32:02 +0530

    (Feat):added ReleaseTicket component

---
+++ src/components/releaseTicket.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Id } from '../../convex/_generated/dataModel'
+import { useMutation } from 'convex/react'
+import { api } from '../../convex/_generated/api'
+import { XCircle } from 'lucide-react'
+
+type Props = {}
+
+const releaseTicket = ({eventId , waitingListId}:{
+    eventId:Id<"events"> ,
+    waitingListId:Id<"waitingList">
+}) => {
+
+      const[isReleasing , setIsReleasing]=useState(false);
+      const releaseTicket= useMutation(api.waitingList.releaseTicket);
+    const handleRelease=async()=>{
+
+        if(!confirm("Are you sure you want to release your ticket"))
+        {
+            return ;
+        }
+        try {
+            setIsReleasing(true);
+            await releaseTicket({eventId , waitingListId});
+        } catch (error) {
+            console.error("Error Releasing Ticket " , error);
+        }
+        finally{
+            setIsReleasing(false);
+        }
+    }
+
+  return (
+    <button
+      onClick={handleRelease}
+      disabled={isReleasing}
+      className='mt-2 w-full flex items-center justify-center gap-2 py-2 px-4 bg-red-100   text-red-700 rounded-lg hover:bg-red-200 transition disabled:opacity-50 disabled:cursor-not-allowed'
+    >
+        <XCircle className='w-4 h-4'/>
+        {isReleasing ?"Releasing":"Release Ticket Offer"}
+
+    </button>
+  )
+}
+
+export default releaseTicket

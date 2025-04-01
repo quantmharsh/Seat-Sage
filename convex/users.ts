ca6841da1e14918638aff4422524fe88a5c65c87
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-01 17:23:23 +0530

    (Feat):added convex api to getStripeConnectId

--- convex/users.ts
+++ convex/users.ts
@@ -67,3 +67,13 @@ export const updateOrCreateUserStripeConnectId= mutation({
 
     }
 }) 
+
+//get seller stripeConnectId
+export const getUsersStripeConnectId=query({
+    args:{userId:v.string()},
+    handler:async(ctx , {userId})=>{
+    const user =await ctx.db.query("users").filter((q)=>q.eq(q.field("userId"), userId)).filter((q)=>q.neq(q.field("stripeConnectId"), undefined)).first();
+    return user?.stripeConnectId;
+
+    }
+});

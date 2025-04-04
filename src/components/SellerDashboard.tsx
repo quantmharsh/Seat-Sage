0948df27bb4ae022ca8131a3e898b99386c49181
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-05 00:34:36 +0530

    (Feat):implemented useEffect to fetch account status

--- src/components/SellerDashboard.tsx
+++ src/components/SellerDashboard.tsx
@@ -1,6 +1,11 @@
 "use client";
-import React, { useState } from 'react'
-import { AccountStatus } from '../../actions/getStripeConnectAccountStatus';
+import React, { useEffect, useState } from 'react'
+import { AccountStatus, getStripeConnectAccountStatus } from '../../actions/getStripeConnectAccountStatus';
+import { useRouter } from 'next/navigation';
+import { useUser } from '@clerk/nextjs';
+import { useQuery } from 'convex/react';
+import { api } from '../../convex/_generated/api';
+import Spinner from './Spinner';
 
 type Props = {}
 
@@ -9,7 +14,32 @@ const SellerDashboard = (props: Props) => {
   const[accountLinkCreatePending , setAccountLinkCreatePending]=useState(false);
   const[error , setError]=useState(false);
   const[accountStatus ,setAccountStatus]=useState<AccountStatus | null>(null);
-
+  const router=useRouter();
+  const {user}=useUser();
+  const stripeConnectId=useQuery(api.users.getUsersStripeConnectId,{
+    userId:user?.id ||"",
+  });
+  const isReadyToAcceptPayments=accountStatus?.isActive && accountStatus?.payoutsEnabled;
+  useEffect(()=>{
+    if(stripeConnectId)
+    {
+    fetchAccountStatus();
+    }
+  },[stripeConnectId]);
+  if(stripeConnectId===undefined)
+  {
+    return <Spinner/>;
+  }
+  const fetchAccountStatus=async()=>{
+    if(stripeConnectId)
+    {
+      try {
+        const status= await  getStripeConnectAccountStatus(stripeConnectId);
+      } catch (error) {
+        console.error("Error fetching account status:");
+      }
+    }
+  }
   return (
 
     <div>SellerDashboard</div>

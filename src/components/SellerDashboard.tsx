e0648619edb605859bb771e7710d1efeb1787e78
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-07 16:27:26 +0530

    (Feat):added Header seller dashboard

--- src/components/SellerDashboard.tsx
+++ src/components/SellerDashboard.tsx
@@ -6,6 +6,7 @@ import { useUser } from '@clerk/nextjs';
 import { useQuery } from 'convex/react';
 import { api } from '../../convex/_generated/api';
 import Spinner from './Spinner';
+import { createStripeConnectLoginLink } from '../../actions/createStripeConnectLoginLink';
 
 type Props = {}
 
@@ -16,6 +17,8 @@ const SellerDashboard = (props: Props) => {
   const[accountStatus ,setAccountStatus]=useState<AccountStatus | null>(null);
   const router=useRouter();
   const {user}=useUser();
+
+  //get stripeconnect Id from db;
   const stripeConnectId=useQuery(api.users.getUsersStripeConnectId,{
     userId:user?.id ||"",
   });
@@ -35,14 +38,41 @@ const SellerDashboard = (props: Props) => {
     {
       try {
         const status= await  getStripeConnectAccountStatus(stripeConnectId);
+        setAccountStatus(status);
       } catch (error) {
         console.error("Error fetching account status:");
       }
     }
   }
+  const handleManageAccount=async()=>{
+    try {
+      if(stripeConnectId && accountStatus?.isActive)
+      {
+        const loginUrl=await createStripeConnectLoginLink(stripeConnectId);
+        window.location.href=loginUrl;
+      }
+    } catch (error) {
+      console.error("Error accessing stripe Connect portal:" ,error);
+      setError(true);
+    }
+  }
   return (
 
-    <div>SellerDashboard</div>
+      <div className='max-w-3xl mx-auto p-6'>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          {/* Header Section */}
+          <div className='bg-gradient-to-r from-amber-400 to-amber-600 px-6 py-8 text-white'>
+            <h2 className='text-2xl font-bold '>
+      Seller Dashboard
+            </h2>
+            <p>
+      Manage your seller profile and payment settings
+            </p>
+          </div>
+
+        </div>
+
+      </div>
   )
 }
 

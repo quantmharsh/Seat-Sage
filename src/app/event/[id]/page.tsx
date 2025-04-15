77e2943c2eea13774faf0081bff5798dc06ab5eb
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-15 21:17:42 +0530

    (Feat):added return ,refresh url end points

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -15,107 +15,106 @@ import JoinQueue from '@/components/JoinQueue'
 
 type Props = {}
 
-const EventPage= () => {
+const EventPage = () => {
 
-    const{user}=useUser();
-    const params=useParams();
-    const event=useQuery(api.events.getById,{
-        eventId:params.id as Id<"events">
-    })
-    const availability= useQuery(api.events.getEventAvaliability,{
-      eventId:params.id as Id<"events">,
-    });
-    //get image
-    const imageUrl=useStorageUrl(event?.imageStorageId);
-    if(!event ||!availability)
-    {
-      return (
-        <div className='min-h-screen flex items-center justify-center'>
-          <Spinner/>
-        </div>
-      );
-    }
+  const { user } = useUser();
+  const params = useParams();
+  const event = useQuery(api.events.getById, {
+    eventId: params.id as Id<"events">
+  })
+  const availability = useQuery(api.events.getEventAvaliability, {
+    eventId: params.id as Id<"events">,
+  });
+  //get image
+  const imageUrl = useStorageUrl(event?.imageStorageId);
+  if (!event || !availability) {
+    return (
+      <div className='min-h-screen flex items-center justify-center'>
+        <Spinner />
+      </div>
+    );
+  }
   return (
-     <div  className='min-h-screen bg-gray-50'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
-          <div className='bg-white rounded-xl shadow-sm overflow-hidden'>
-          {imageUrl &&(
-          <div className='aspect-[21/9] relative w-full'>
-            <Image
-           src={imageUrl}
-           alt={event.name}
-           fill 
-           className='object-cover'
-           priority 
-            />
+    <div className='min-h-screen bg-gray-50'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+        <div className='bg-white rounded-xl shadow-sm overflow-hidden'>
+          {imageUrl && (
+            <div className='aspect-[21/9] relative w-full'>
+              <Image
+                src={imageUrl}
+                alt={event.name}
+                fill
+                className='object-cover'
+                priority
+              />
             </div>
-         )}
+          )}
 
-         <div className='p-8'>
-          <div className='grid grid-cols-1 lg:grid-cols-2 gap-12'>
-            {/* Left column-event details */}
-            <div className='space-y-8'>
-              <div>
-                <h1 className='text-4xl font-bold text-gray-900 mb-4'>
-                  {event.name}
-                </h1>
-                <p className='text-lg text-gray-600 '>
-                  {event.description}
-                </p>
-              </div>
-              <div className='grid grid-cols-2 gap-6'>
-                <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
-                  <div className='flex items-center text-gray-600  mb-1'>
-                    <CalendarRangeIcon className='w-5 h-5 mr-2 text-blue-600'/>
-                    <span className='text-sm font-medium'>
-                      Date
-                    </span>
-
-                  </div>
-                  <p className='text-gray-900'>
-                    {new Date(event.eventDate).toLocaleDateString()}
+          <div className='p-8'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-12'>
+              {/* Left column-event details */}
+              <div className='space-y-8'>
+                <div>
+                  <h1 className='text-4xl font-bold text-gray-900 mb-4'>
+                    {event.name}
+                  </h1>
+                  <p className='text-lg text-gray-600 '>
+                    {event.description}
                   </p>
                 </div>
-                <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
-                  <div className='flex items-center text-gray-600  mb-1'>
-                    <Ticket className='w-5 h-5 mr-2 text-blue-600'/>
-                    <span className='text-sm font-medium'>
-                      Price
-                    </span>
+                <div className='grid grid-cols-2 gap-6'>
+                  <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
+                    <div className='flex items-center text-gray-600  mb-1'>
+                      <CalendarRangeIcon className='w-5 h-5 mr-2 text-blue-600' />
+                      <span className='text-sm font-medium'>
+                        Date
+                      </span>
 
+                    </div>
+                    <p className='text-gray-900'>
+                      {new Date(event.eventDate).toLocaleDateString()}
+                    </p>
                   </div>
-                  <p className='text-gray-900'>
-                    {event.price.toFixed(2)}
-                  </p>
-                </div>
-                <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
-                  <div className='flex items-center text-gray-600  mb-1'>
-                    <Users className='w-5 h-5 mr-2 text-blue-600'/>
-                    <span className='text-sm font-medium'>
-                      Availability
-                    </span>
+                  <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
+                    <div className='flex items-center text-gray-600  mb-1'>
+                      <Ticket className='w-5 h-5 mr-2 text-blue-600' />
+                      <span className='text-sm font-medium'>
+                        Price
+                      </span>
 
+                    </div>
+                    <p className='text-gray-900'>
+                      {event.price.toFixed(2)}
+                    </p>
                   </div>
-                  <p className='text-gray-900'>
-                  {availability.totalTickets - availability.purchasedCount}{" "}
-                  / {availability.totalTickets} left
-                  </p>
-                </div>
-                <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
-                  <div className='flex items-center text-gray-600  mb-1'>
-                    <MapPinnedIcon className='w-5 h-5 mr-2 text-blue-600'/>
-                    <span className='text-sm font-medium'>
-                      Location
-                    </span>
+                  <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
+                    <div className='flex items-center text-gray-600  mb-1'>
+                      <Users className='w-5 h-5 mr-2 text-blue-600' />
+                      <span className='text-sm font-medium'>
+                        Availability
+                      </span>
 
+                    </div>
+                    <p className='text-gray-900'>
+                      {availability.totalTickets - availability.purchasedCount}{" "}
+                      / {availability.totalTickets} left
+                    </p>
                   </div>
-                  <p className='text-gray-900'>
-                    {event.location}
-                  </p>
+                  <div className='bg-gray-50 hover:bg-amber-200 p-4 rounded-lg  border border-gray-100'>
+                    <div className='flex items-center text-gray-600  mb-1'>
+                      <MapPinnedIcon className='w-5 h-5 mr-2 text-blue-600' />
+                      <span className='text-sm font-medium'>
+                        Location
+                      </span>
+
+                    </div>
+                    <p className='text-gray-900'>
+                      {event.location}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              {/* Additional Event Information */}
-              <div className="bg-blue-50 border border-blue-100 rounded-lg p-6">
+                {/* Additional Event Information */}
+                <div className="bg-blue-50 border border-blue-100 rounded-lg p-6">
                   <h3 className="text-lg font-semibold text-blue-900 mb-2">
                     Event Information
                   </h3>
@@ -125,13 +124,13 @@ const EventPage= () => {
                     <li>• Age restriction: 18+</li>
                   </ul>
                 </div>
-            </div>
+              </div>
 
-            <div>
-            <div className="sticky top-8 space-y-4">
+              <div>
+                <div className="sticky top-8 space-y-4">
                   <EventCard eventId={params.id as Id<"events">} />
                   {user ? (
-                   <JoinQueue eventId={params.id as Id<"events">} userId={user.id}/>
+                    <JoinQueue eventId={params.id as Id<"events">} userId={user.id} />
                   ) : (
                     <SignInButton>
                       <Button className="w-full bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-700 hover:to-blue-900 text-white font-medium py-2 px-4 rounded-lg transition-all duration-200 shadow-md hover:shadow-lg">
@@ -139,15 +138,15 @@ const EventPage= () => {
                       </Button>
                     </SignInButton>
                   )}
+                </div>
+
+
+              </div>
             </div>
-            
-            
-          </div>
-         </div>
           </div>
         </div>
-     </div>
-     </div>
+      </div>
+    </div>
 
   )
 }
--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -13,39 +13,38 @@ import PurchaseTicket from './PurchaseTicket';
 
 
 
-const EventCard = ({eventId}:{
-    eventId:Id<"events">
+const EventCard = ({ eventId }: {
+  eventId: Id<"events">
 }) => {
 
-  const {user}=useUser();
-  const router=useRouter();
-  const event =useQuery(api.events.getById , {
+  const { user } = useUser();
+  const router = useRouter();
+  const event = useQuery(api.events.getById, {
     eventId
   });
   //get avaliability of tickets
-  const availability= useQuery(api.events.getEventAvaliability , {
+  const availability = useQuery(api.events.getEventAvaliability, {
     eventId
   });
   // Check  wheteher user has purchased ticket for this event or not
-  const userTicket=useQuery(api.tickets.getUserTicketForEvent , {
-      eventId  , 
-      userId:user?.id ?? "",
+  const userTicket = useQuery(api.tickets.getUserTicketForEvent, {
+    eventId,
+    userId: user?.id ?? "",
   })
 
-  const queuePosition=useQuery(api.waitingList.getQueuePosition , {
-    eventId ,
+  const queuePosition = useQuery(api.waitingList.getQueuePosition, {
+    eventId,
     userId: user?.id ?? "",
   });
-  const imageUrl= useStorageUrl(event?.imageStorageId);
+  const imageUrl = useStorageUrl(event?.imageStorageId);
 
-  if(!event  ||!availability)
-  {
-    return  null ;
+  if (!event || !availability) {
+    return null;
   }
-  
-  const isPastEvent= event?.eventDate <=Date.now();
-  const isEventOwner= event?.userId ===user?.id;
-  console.log("isEventOwner" , isEventOwner);
+
+  const isPastEvent = event?.eventDate <= Date.now();
+  const isEventOwner = event?.userId === user?.id;
+  console.log("isEventOwner", isEventOwner);
 
 
   const renderQueuePosition = () => {
@@ -92,168 +91,165 @@ const EventCard = ({eventId}:{
       </div>
     );
   };
-   
-//    Render Ticket Status. whether owner of event , user reserver it , or user already holds the ticket , number  of people in queue
 
-const renderTicketStatus=()=>{
-  if(!user)
-  {
-    return null;
-  }
-  //if owner of event
-  if(isEventOwner)
-  {
-    return (
-      <div className=' mt-4'>
-        <button onClick={(e)=> {e.stopPropagation()
-router.push(`/seller/events/${eventId}/edit`);
+  //    Render Ticket Status. whether owner of event , user reserver it , or user already holds the ticket , number  of people in queue
 
-        }}
-         className='w-full bg-purple-100 text-gray-700  px-6 py-3 rounded-lg font-medium hover:bg-purple-200  transition-colors duration-200 shadow-sm flex items-center justify-center gap-2'
-        >
-          <PencilIcon className='w-5 h-5'/>
-          Edit Event
-        </button>
-      </div>
-    );
-  }
+  const renderTicketStatus = () => {
+    if (!user) {
+      return null;
+    }
+    //if owner of event
+    if (isEventOwner) {
+      return (
+        <div className=' mt-4'>
+          <button onClick={(e) => {
+            e.stopPropagation()
+            router.push(`/seller/events/${eventId}/edit`);
 
-  if(userTicket)
-  {
-    return (
-      <div className="mt-4 flex items-center justify-between p-3 bg-green-50 rounded-lg border border-green-100">
-        <div className="flex items-center">
-        <CheckCircle className="w-5 h-5 text-green-600 mr-2"/>
-        <span className="text-green-700 font-medium">
-        You have a ticket 😀
-        </span>
-      </div>
-      <button    onClick={() => router.push(`/tickets/${userTicket._id}`)}
-            className="text-sm bg-green-600 hover:bg-green-700 text-white px-3 py-1.5 rounded-full font-medium shadow-sm transition-colors duration-200 flex items-center gap-1">
-        View your ticket
-      </button>
-      </div>
-    )
-  }
-  if (queuePosition) {
-    return (
-      <div className="mt-4">
-        {queuePosition.status === "offered" && (
-          <PurchaseTicket eventId={eventId} />
-        
-        )}
-        {renderQueuePosition()}
-        {queuePosition.status === "expired" && (
-          <div className="p-3 bg-red-50 rounded-lg border border-red-100">
-            <span className="text-red-700 font-medium flex items-center">
-              <XCircle className="w-5 h-5 mr-2" />
-              Offer expired
+          }}
+            className='w-full bg-purple-100 text-gray-700  px-6 py-3 rounded-lg font-medium hover:bg-purple-200  transition-colors duration-200 shadow-sm flex items-center justify-center gap-2'
+          >
+            <PencilIcon className='w-5 h-5' />
+            Edit Event
+          </button>
+        </div>
+      );
+    }
+
+    if (userTicket) {
+      return (
+        <div className="mt-4 flex items-center justify-between p-3 bg-green-50 rounded-lg border border-green-100">
+          <div className="flex items-center">
+            <CheckCircle className="w-5 h-5 text-green-600 mr-2" />
+            <span className="text-green-700 font-medium">
+              You have a ticket 😀
             </span>
           </div>
-        )}
-      </div>
-    );
+          <button onClick={() => router.push(`/tickets/${userTicket._id}`)}
+            className="text-sm bg-green-600 hover:bg-green-700 text-white px-3 py-1.5 rounded-full font-medium shadow-sm transition-colors duration-200 flex items-center gap-1">
+            View your ticket
+          </button>
+        </div>
+      )
+    }
+    if (queuePosition) {
+      return (
+        <div className="mt-4">
+          {queuePosition.status === "offered" && (
+            <PurchaseTicket eventId={eventId} />
+
+          )}
+          {renderQueuePosition()}
+          {queuePosition.status === "expired" && (
+            <div className="p-3 bg-red-50 rounded-lg border border-red-100">
+              <span className="text-red-700 font-medium flex items-center">
+                <XCircle className="w-5 h-5 mr-2" />
+                Offer expired
+              </span>
+            </div>
+          )}
+        </div>
+      );
+    }
+    return null;
+
   }
-return null;
- 
-}
-   
+
 
   return (
-    <div  onClick={()=> router.push(`/event/${eventId}`)}
-     className={`bg-white rounded-xl shadow-sm hover:shadow-lg transition-all duration-300 border border-gray-100 cursor-pointer overflow-hidden relative ${isPastEvent ?"opacity-75 hover:opacity-100 ":""} `}
+    <div onClick={() => router.push(`/event/${eventId}`)}
+      className={`bg-white rounded-xl shadow-sm hover:shadow-lg transition-all duration-300 border border-gray-100 cursor-pointer overflow-hidden relative ${isPastEvent ? "opacity-75 hover:opacity-100 " : ""} `}
     >
       {/* Event image */}
       {imageUrl && (
         <div className='relative w-full h-48'>
           <Image src={imageUrl}
-           alt={event.name}
-           fill
-           className='object-cover'
-           priority
+            alt={event.name}
+            fill
+            className='object-cover'
+            priority
           />
-          <div className=' absolute inset-0 bg-gradient-to-t from-black/50 to-transparent'/>
-          </div>
+          <div className=' absolute inset-0 bg-gradient-to-t from-black/50 to-transparent' />
+        </div>
       )}
-      <div  className={`p-6${imageUrl ?"relative":""}`} >
-           <div className='flex justify-between items-start'>
-            <div>
-              <div className=' flex flex-col items-start gap-2'>
-                {isEventOwner && (
-                  <span className='inline-flex items-center ap-1 bg-blue-600/90 text-white px-2 py-1 rounded-full text-xs font-medium '> 
-                  <StarIcon className=' w-3 h-3'/>
-                    Your Event 
-                  </span>
-                )}
-                <h2 className='text-2xl  font-bold text-gray-900 '> {event.name}</h2>
-
-              </div>
-              {isPastEvent &&(
-                <span className='inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 mt-2 '>
-                    Past Event
+      <div className={`p-6${imageUrl ? "relative" : ""}`} >
+        <div className='flex justify-between items-start'>
+          <div>
+            <div className=' flex flex-col items-start gap-2'>
+              {isEventOwner && (
+                <span className='inline-flex items-center ap-1 bg-blue-600/90 text-white px-2 py-1 rounded-full text-xs font-medium '>
+                  <StarIcon className=' w-3 h-3' />
+                  Your Event
                 </span>
               )}
-            </div>
+              <h2 className='text-2xl  font-bold text-gray-900 '> {event.name}</h2>
 
-            {/* Price tag*/}
-            <div className='flex flex-col items-end gap-2 ml-4'>
-              <span className={`px-4 py-1.5 font-semibold rounded-full ${
-                isPastEvent
-                  ? "bg-gray-50 text-gray-500"
-                  : "bg-green-50 text-green-700"
+            </div>
+            {isPastEvent && (
+              <span className='inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 mt-2 '>
+                Past Event
+              </span>
+            )}
+          </div>
+
+          {/* Price tag*/}
+          <div className='flex flex-col items-end gap-2 ml-4'>
+            <span className={`px-4 py-1.5 font-semibold rounded-full ${isPastEvent
+                ? "bg-gray-50 text-gray-500"
+                : "bg-green-50 text-green-700"
               }`}>
-                 Rs.{event.price.toFixed(2)}
+              Rs.{event.price.toFixed(2)}
+            </span>
+            {availability.purchasedCount >= availability.totalTickets && (
+              <span className='px-4 py-1.5 bg-red-50 text-red-700 font-semibold rounded-full text-sm'>
+                Sold Out
               </span>
-              {availability.purchasedCount >=  availability.totalTickets && (
-                <span className='px-4 py-1.5 bg-red-50 text-red-700 font-semibold rounded-full text-sm'>
-                  Sold Out 
+            )}
+
+
+          </div>
+
+        </div>
+        <div className='mt-4 space-y-3'>
+          <div className=' flex items-center text-gray-600'>
+            <MapPin className='w-4 h-4 mr-2' />
+            <span>
+              {event.location}
+            </span>
+          </div>
+          <div className='flex items-center text-gray-600'>
+            <CalendarDays className='w-4 h-4 mr-2' />
+            <span>
+              {new Date(event.eventDate).toLocaleDateString()}{" "}
+              {isPastEvent && "(Ended)"}
+            </span>
+          </div>
+
+          <div>
+            <TicketsPlane />
+            <span>
+              {availability.totalTickets - availability.purchasedCount} /{" "}
+              {availability.totalTickets} available
+              {/*  render total ticket reserved by diffrent peoples */}
+              {!isPastEvent && availability.activeOffers > 0 && (
+                <span className=' text-amber-600 text-sm ml-2'>
+                  ({availability.activeOffers}{" "}
+
+                  {availability.activeOffers === 1 ? "person" : "people"} trying to buy)
                 </span>
               )}
+            </span>
+          </div>
 
 
-            </div>
-
-           </div>
-           <div className='mt-4 space-y-3'>
-             <div className=' flex items-center text-gray-600'>
-              <MapPin className='w-4 h-4 mr-2'/>
-              <span>
-                {event.location}
-              </span>
-             </div>
-             <div className='flex items-center text-gray-600'>
-              <CalendarDays className='w-4 h-4 mr-2'/>
-              <span>
-                {new Date(event.eventDate).toLocaleDateString()}{" "}
-                {isPastEvent  && "(Ended)"}
-              </span>
-             </div>
-
-             <div>
-              <TicketsPlane/>
-              <span>
-                {availability.totalTickets - availability.purchasedCount} /{" "}
-                {availability.totalTickets} available 
-                {/*  render total ticket reserved by diffrent peoples */}
-                {!isPastEvent  && availability.activeOffers > 0 &&(
-                  <span className=' text-amber-600 text-sm ml-2'>
-                    ({availability.activeOffers}{" "}
-                    
-                    {availability.activeOffers===1?"person":"people"} trying to buy)
-                    </span>
-                )}
-              </span>
-             </div>
-
-              
-           </div>
-           <p className=' mt-4 text-gray-600 text-sm line-clamp-2'>
-            {event.description}
-           </p>
-           {/*We Want to render ticket status . so need to  stop propogation . ( on click  dont redirect to  event details page . but  render ticket status . ) */}
-           <div onClick={(e)=>e.stopPropagation()}>
-               {!isPastEvent && renderTicketStatus() }
-           </div>
+        </div>
+        <p className=' mt-4 text-gray-600 text-sm line-clamp-2'>
+          {event.description}
+        </p>
+        {/*We Want to render ticket status . so need to  stop propogation . ( on click  dont redirect to  event details page . but  render ticket status . ) */}
+        <div onClick={(e) => e.stopPropagation()}>
+          {!isPastEvent && renderTicketStatus()}
+        </div>
       </div>
 
     </div>

7d0a285184eb379ab34fc2ba56414656a5684c95
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-18 01:25:25 +0530

    (Feat):added GetTicketWithDetails api to fetch single ticket details

--- convex/tickets.ts
+++ convex/tickets.ts
@@ -3,15 +3,34 @@ import { query } from "./_generated/server";
 
 
 
-export const getUserTicketForEvent=query({
+export const getUserTicketForEvent = query({
 
-    args:{
-        userId:v.string(),
-        eventId:v.id("events"),
+    args: {
+        userId: v.string(),
+        eventId: v.id("events"),
     },
-    handler:async(ctx , {userId , eventId})=>{
-         const ticket= await ctx.db.query("tickets").withIndex("by_user_event" , (q)=>q.eq("userId" , userId).eq("eventId",eventId)).first();
-         return  ticket;
+    handler: async (ctx, { userId, eventId }) => {
+        const ticket = await ctx.db.query("tickets").withIndex("by_user_event", (q) => q.eq("userId", userId).eq("eventId", eventId)).first();
+        return ticket;
 
     }
 })
+
+//get event details for particulat ticket  detail .
+
+export const getTicketWithDetails = query({
+    args: {
+        ticketId: v.id("tickets")
+    },
+    handler: async (ctx, { ticketId }) => {
+        const ticket = await ctx.db.get(ticketId);
+        if (!ticket) {
+            return null;
+        }
+        const event = await ctx.db.get(ticket.eventId);
+        return {
+            ...ticket,
+            event
+        }
+    }
+});

4d4000610a6fda8c89a285ec3f97fbbc0681a4bb
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-26 22:10:55 +0530

    (Feat):added My Events Page for sellers

--- src/app/seller/events/page.tsx
+++ src/app/seller/events/page.tsx
@@ -1,4 +1,7 @@
 
+import SellerEventList from '@/components/SellerEventList';
+import { ArrowLeftFromLine, PlusCircle } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
 type Props = {}
@@ -6,7 +9,43 @@ type Props = {}
 const SellerEventsPage = (props: Props) => {
     
   return (
-    <div>page</div>
+   <div className='min-h-screen bg-gray-50'>
+    <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8'>
+      {/* Header */}
+      <div className='bg-gradient-to-r from-amber-400 to-amber-600 rounded-xl shadow-sm border border-gray-200 p-6 mb-8'>
+        <div className='flex flex-col  md:flex-row items-start md:items-center  justify-between gap-4'>
+          <div className='flex items-center gap-4'>
+            <Link
+
+            href={'/seller'}
+            className='text-gray-500  hover:text-gray-700 transition-colors'
+            >
+             <ArrowLeftFromLine className='w-5 h-5'/>
+            </Link>
+            <div>
+              <h1 className='text-2xl font-bold text-gray-900'>
+                My Events
+              </h1>
+              <p className='mt-1 text-black  font-mono'>
+                Manage your event listings and track sales
+              </p>
+            </div>
+          </div>
+          <Link
+          href={'/seller/new-event'}
+           className="flex items-center justify-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors"
+          >
+            <PlusCircle className='w-5 h-5'/>
+            Create Event
+          </Link>
+        </div>
+        </div>
+        {/* Event list created by   user(seller) */}
+        <div className='bg-white rounded-xl shadow-sm border-gray-200 p-6'>
+          <SellerEventList/>
+        </div>
+    </div>
+   </div>
   )
 }
 
---
+++ src/components/SellerEventList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SellerEventList = (props: Props) => {
+  return (
+    <div>SellerEventList</div>
+  )
+}
+
+export default SellerEventList

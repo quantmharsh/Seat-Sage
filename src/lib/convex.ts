5a7bbf245c276e13310db33428ec2af09abb1424
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-26 18:42:12 +0530

    (Feat):added few comments

--- src/lib/convex.ts
+++ src/lib/convex.ts
@@ -1,6 +1,6 @@
 import { ConvexHttpClient } from "convex/browser"
 
-
+//function to  get convex client. basically get public convex url to access db and all mutations , query 
 export const getConvexClient=()=>{
 
     if(!process.env.NEXT_PUBLIC_CONVEX_URL)

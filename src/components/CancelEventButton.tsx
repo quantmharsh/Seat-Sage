1010979ea87770047eda6ebb3ed2f131a13e863a
Harsh Srivastava <srivastavah240@gmail.com>
2025-04-30 19:12:40 +0530

    (Feat):added SellerEventCard function , created CancelEventButton Component

---
+++ src/components/CancelEventButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Id } from '../../convex/_generated/dataModel'
+
+type Props = {}
+
+const CancelEventButton = ({eventId}: {eventId:Id<"events">}) => {
+  return (
+    <div>CancelEventButton</div>
+  )
+}
+
+export default CancelEventButton;
